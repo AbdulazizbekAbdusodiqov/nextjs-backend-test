a7b9d671f8bb907d4b20e4f1c29a568d4d3994e2
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-06-18 16:23:09 +0500

    ok

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
         toast.info("password is required")
         return
       }
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
--- app/register/index.tsx
+++ app/register/index.tsx
@@ -34,7 +34,7 @@ const RegisterPage = () => {
                 return
             }
 
-            const res = await fetch("http://localhost:3000/api/auth/register", {
+            const res = await fetch("/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
